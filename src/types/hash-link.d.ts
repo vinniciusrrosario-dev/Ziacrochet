cc3bb706847dc06ab9dacbcad727cb85d451d0f7
vinniciusrrosario-dev <vinniciusrrosario.com>
2025-11-19 08:12:37 -0300

    Corrigindo erro de tipagem HashLink e removendo conflito de scroll (Tela Branca)

---
+++ src/types/hash-link.d.ts
@@ -0,0 +1,4 @@
+// Declaração de Módulo para 'react-router-hash-link'
+// Isso resolve o erro "Cannot find module" quando a biblioteca
+// não tem tipos em @types ou o TypeScript não consegue resolvê-los.
+declare module 'react-router-hash-link';
